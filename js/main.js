23038cb1749a58e53f4962c3f558ab2460c95e53
Igorth <igordiasth@gmail.com>
2020-12-04 14:57:30 +1300

    remove scroll top

--- js/main.js
+++ js/main.js
@@ -49,18 +49,3 @@ gsap.from('.transition3', {
     duration: 1.2,
     stagger: .6
 })
-
-var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
